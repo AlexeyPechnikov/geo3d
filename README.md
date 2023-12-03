0b2dcce756ee03fc4d5dd606238d63529903cc52
Alexey Pechnikov <pechnikov@mobigroup.ru>
2023-12-03 22:08:15 +0700

    Load the updated geomed3dv5 library code where statistics are limited to the density function.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# geomed3d
-GeoMed3D (Geologic Medium 3D) - 3D Density Inversion by Circular Hough Transform for Everyone
+# GeoMed3D (Geologic Medium 3D) - 3D Density Inversion by Circular Hough Transform for Everyone
+<img src="https://github.com/mobigroup/geomed3d/assets/7342379/c585eace-5669-42a7-b01b-28dff8820461">
+
---
+++ setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# ----------------------------------------------------------------------------
+# GeoMed3d
+# 
+# This file is part of the GeoMed3D project: https://github.com/mobigroup/geomed3d
+# 
+# Copyright (c) 2023, Alexey Pechnikov
+# 
+# Licensed under the BSD 3-Clause License (see LICENSE for details)
+# ----------------------------------------------------------------------------
+
+from setuptools import setup
+import urllib.request
+
+# read the contents of your README file
+#from pathlib import Path
+#this_directory = Path(__file__).parent
+#long_description = (this_directory / "README.md").read_text()
+
+upstream_url = 'https://raw.githubusercontent.com/mobigroup/geomed3d/main/README.md'
+response = urllib.request.urlopen(upstream_url)
+long_description = response.read().decode('utf-8')
+
+setup(
+    name='geomed3d',
+    version='2023.12.03',
+    description='GeoMed3D (Geologic Medium 3D) - 3D Density Inversion by Circular Hough Transform for Everyone',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/mobigroup/geomed3d',
+    author='Alexey Pechnikov',
+    author_email='pechnikov@mobigroup.ru',
+    license='BSD-3-Clause',
+    packages=['geomed3d'],
+    install_requires=['xarray>=0.19.0',
+                      'numpy>=1.22.4',
+                      'numba',
+                      'dask',
+                      'pandas>=1.5',
+                      'geopandas',
+                      'rasterio',
+                      'rioxarray',
+                      'scipy>=1.9.1',
+                      'shapely>=2.0.1',
+                      'rioxarray',
+                      'h5netcdf>=1.2.0',
+                      'h5py',
+                      'zipfile',
+                      'earthengine-api',
+                      'vtk'
+                      ],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.10'
+    ],
+    python_requires='>=3.10'
+)
